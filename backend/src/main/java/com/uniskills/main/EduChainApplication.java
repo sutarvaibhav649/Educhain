f191c7e0020d5b74b24c9430ace73b8c6736a1e8
Vaibhav Sutar <sutar.vaibhav649@gmail.com>
2025-10-12 16:21:11 +0530

    setup of the prjects educhain

---
+++ backend/src/main/java/com/uniskills/main/EduChainApplication.java
@@ -0,0 +1,13 @@
+package com.uniskills.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduChainApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/uniskills/main/EduChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniskills.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
